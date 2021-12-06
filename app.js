e28952abea6bf5e92abc532875d80b28ea1235ae
Alex Blair <bclan11@gmail.com>
2021-12-06 15:22:23 -0800

    initial commit

--- app.js
+++ app.js
@@ -11,47 +11,51 @@ const teamTwoSubtractButton = document.getElementById('team-two-subtract-button'
 const finishGameButton = document.getElementById('finish-game-button');
 const teamOneLabel = document.getElementById('team-one-name');
 const teamTwoLabel = document.getElementById('team-two-name');
+const teamOneScore = document.getElementById('team-one-score');
+const teamTwoScore = document.getElementById('team-two-score');
 
 // create an array to hold on to the state of past games
+let pastGamesArray = [];
+let name1 = '';
+let name2 = '';
+let score1 = 0;
+let score2 = 0;
 
-let name1 = '',
-let name2 =  '',
-let score1 = 0,
-let score2 = 0,
 
 nameForm.addEventListener('submit', (e) => {
     // don't forget to prevent the default form behavior!
-
+    e.preventDefault();
     // get the name data from the form
-
+    const data = new FormData(nameForm);
     // set the state to this data from the form
-
+    name1 = data.get('team-one');
+    name2 = data.get('team-two');
+    nameForm.reset();
     // reset the form values
-
     displayCurrentGameEl();
 });
 
 
 teamOneAddButton.addEventListener('click', () => {
     // increment the current state for team one's score
-    
+    score1++;
     displayCurrentGameEl();});
 
 teamTwoAddButton.addEventListener('click', () => {
     // increment the current state for team two's score
-
+    score2++;
     displayCurrentGameEl();
 });
 
 teamOneSubtractButton.addEventListener('click', () => {
     // decrement the current state for team one's score
-
+    score1--;
     displayCurrentGameEl();
 });
 
 teamTwoSubtractButton.addEventListener('click', () => {
     // decrement the current state for team two's score
-
+    score2--;
     displayCurrentGameEl();
 });
 
@@ -59,33 +63,64 @@ finishGameButton.addEventListener('click', () => {
     
     // add the current game to an array of games in state.
     // HINT: it will be helpful to keep track of these games as objects with 4 properties, one for each piece of state we're tracking
-
+    const game = makeGame();
+    pastGamesArray.push(game);
     displayAllGames();
 
     // reset the initial state to start with a new form
-
+    resetState();
     displayCurrentGameEl();
 });
 
 
 function displayCurrentGameEl() {
     // clear out the current game div
-
     // change the label to show team one's name;
     // change the label to show team two's name;
+    teamOneLabel.textContent = name1;
+    teamTwoLabel.textContent = name2;
+    teamOneScore.textContent = score1;
+    teamTwoScore.textContent = score2;
+
 
     // call the render game function to create a game element
-    
+    const currentGame = {
+        name1,
+        name2,
+        score1,
+        score2
+    };
+    renderGame(currentGame);
     // append the element to the cleared out current game div
+    
 }
 
 
 function displayAllGames() {
     // clear out the past games list in the DOM
-
+    pastGamesEl.textContent = '';
     // loop through the past games in state
+    for (let games of pastGamesArray) {
+        const container = renderGame(games);
+        pastGamesEl.append(container);
+    }
     // render and append a past game for each past game in state
 }
 
+function makeGame() {
+    return {
+        name1,
+        name2,
+        score1,
+        score2
+    };
+}
 
-displayCurrentGameEl();
+function resetState() {
+    name1 = '';
+    name2 = '';
+    score1 = 0;
+    score2 = 0;
+}
+
+// displayCurrentGameEl();
--- render-utils.js
+++ render-utils.js
@@ -3,7 +3,6 @@ export function renderGame(game) {
     const div = document.createElement('div');
     const team1Div = renderTeam(game.name1, game.score1);
     const team2Div = renderTeam(game.name2, game.score2);
-
     div.append(team1Div, team2Div);
 
     div.classList.add('game');
@@ -15,7 +14,8 @@ export function renderTeam(name, score) {
     const teamDiv = document.createElement('div');
     const nameDiv = document.createElement('p');
     const scoreDiv = document.createElement('p');
-
+    nameDiv.textContent = name;
+    scoreDiv.textContent = score;
     teamDiv.append(nameDiv, scoreDiv);
 
     return teamDiv;
